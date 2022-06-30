6cc5f8525eacfe6509aa9a3b7657864f146fa609
ESedrak <elizabeth_sedrak@hotmail.com>
2022-06-30 10:02:34 +1000

    add logic to reset values back to default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// addTask
-
-const taskName = document.querySelector("#taskHeader");
+// addTask Form
+const taskName = document.querySelector("#taskName");
 const taskDescription = document.querySelector("#taskText");
 const taskAssign = document.querySelector("#taskAssign");
 const dueDate = document.querySelector("#dueDate");
@@ -9,39 +8,40 @@ const taskStatus = document.querySelector("#taskStatus");
 const submitTaskFormBtn = document.querySelector("#submitTaskFormBtn");
 
 // Error spans
-const taskHeaderError = document.querySelector("#taskHeaderError");
+const taskNameError = document.querySelector("#taskNameError");
 const taskDescriptionError = document.querySelector("#taskDescriptionError");
 const taskAssignError = document.querySelector("#taskAssignError");
 const dueDateError = document.querySelector("#dueDateError");
-const taskPriorityError = document.querySelector("#taskPriorityError");
-const taskStatusError = document.querySelector("#taskStatusError");
 
 const validateTaskForm = () => {
   if (taskName.value === "") {
-    taskHeaderError.innerText = "Please add a task name.";
+    taskNameError.innerText = "Please add a task name.";
   } else if (taskName.value.length < 8) {
-    taskHeaderError.innerText =
-      "Task name needs to be longer than 8 characters.";
-  }
-  if (taskDescription.value === "") {
+    taskNameError.innerText = "Task name needs to be longer than 8 characters.";
+  } else if (taskDescription.value === "") {
     taskDescriptionError.innerText = "Please add a task description.";
   } else if (taskDescription.value.length < 15) {
     taskDescriptionError.innerText =
       "Task Description needs to be longer than 15 characters.";
-  }
-  if (taskAssign.value === "") {
+  } else if (taskAssign.value === "") {
     taskAssignError.innerText = "Please assign a name.";
   } else if (taskAssign.value.length < 8) {
     taskAssignError.innerText = "Name needs to be longer than 8 characters.";
-  }
-  if (dueDate.value === "") {
+  } else if (dueDate.value === "") {
     dueDateError.innerText = "Please assign a date.";
-  }
-  if ((taskPriority.value = "default")) {
-    taskPriorityError.innerText = "Please choose priority level.";
-  }
-  if ((taskStatus.value = "default")) {
-    taskStatusError.innerText = "Please choose task status.";
+  } else {
+    // reset everything once submitted successfully
+    taskName.value = "";
+    taskDescription.value = "";
+    taskAssign.value = "";
+    dueDate.value = "";
+    taskPriority.value = "low";
+    taskStatus.value = "toDo";
+
+    taskNameError.innerText = "";
+    taskDescriptionError.innerText = "";
+    taskAssignError.innerText = "";
+    dueDateError.innerText = "";
   }
 };
 
@@ -57,7 +57,7 @@ const interval = setInterval(() => {
   // Get current time
   const now = new Date();
 
-  // Format date as in mm/dd/aaaa hh:ii:ss
+  // Format date as in dd//aaaa hh:ii:ss
   const dateTime =
     zeroFill(now.getUTCDate()) +
     "/" +
