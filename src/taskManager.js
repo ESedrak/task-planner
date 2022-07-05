4233b2347e0b690469f8c8d7ef3b805074fac92d
eyetech2dev <eyetech2dev@gmail.com>
2022-07-05 10:40:47 +1000

    Add logic to fix duplication of render method

--- src/taskManager.js
+++ src/taskManager.js
@@ -2,6 +2,7 @@ class TaskManager {
   constructor(currentId = 1) {
     this.currentId = currentId;
     this.tasks = [];
+    this.DOMrender = false;
   }
 
   addTask(
@@ -20,16 +21,19 @@ class TaskManager {
       dueDate: dueDate,
       taskPriority: taskPriority,
       taskStatus: taskStatus,
+      DOMrender: this.DOMrender
     };
     this.tasks.push(task);
   }
 
   render() {
     this.tasks.forEach((task) => {
-      console.log(task);
       let z = document.createElement("div");
+      if(task.DOMrender === false){
       z.innerHTML = createCard(task);
       document.body.appendChild(z);
+      task.DOMrender = !task.DOMrender;
+    }
 
       if (taskStatus.value === "toDo") {
         document.querySelector("#toDoList").appendChild(z);
